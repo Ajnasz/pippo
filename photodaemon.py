bbbfb10e9a87bd92786eeb9cb1379e500ac9d791
Lajos Koszti <ajnasz@ajnasz.hu>
2016-03-07 22:21:41 +0100

    Cosmetics: use functions

--- photodaemon.py
+++ photodaemon.py
@@ -6,11 +6,6 @@ import time
 import json
 import os
 
-with open(os.path.join(os.path.dirname(__file__), 'config.json')) as data_file:
-    config = json.load(data_file)
-
-print config
-
 def take_photo():
     print "%s Capture photo" % (time.strftime('%Y.%m.%d %H:%M:%S %Z'))
     camera = picamera.PiCamera()
@@ -22,13 +17,23 @@ def take_photo():
     r.publish('photo', time.time())
     print "%s Capture done" % (time.strftime('%Y.%m.%d %H:%M:%S %Z'))
 
-r = redis.StrictRedis(host=config['host'], port=config['port'], db=config['db'])
-p = r.pubsub()
-p.subscribe('take-photo')
+def get_config():
+    with open(os.path.join(os.path.dirname(__file__), 'config.json')) as data_file:
+        return json.load(data_file)
 
-while True:
-    message = p.get_message()
-    if message and message['type'] == 'message':
-        take_photo()
+def main():
+    config = get_config()
 
-    time.sleep(0.1)
+    r = redis.StrictRedis(host=config['host'], port=config['port'], db=config['db'])
+    p = r.pubsub()
+    p.subscribe('take-photo')
+
+    while True:
+        message = p.get_message()
+        if message and message['type'] == 'message':
+            take_photo()
+
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
