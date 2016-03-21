fc610c7dadf01591cee6fd96c9ce577bfff21ced
Lajos Koszti <ajnasz@ajnasz.hu>
2016-03-21 08:03:09 +0100

    Allow to unlock on exception

--- feed.py
+++ feed.py
@@ -60,41 +60,44 @@ def islocked():
 
 def main():
 	lock()
-	# Parse command line parameters.
-	sensor_args = { '11': Adafruit_DHT.DHT11,
-					'22': Adafruit_DHT.DHT22,
-					'2302': Adafruit_DHT.AM2302 }
-	if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-		sensor = sensor_args[sys.argv[1]]
-		pin = sys.argv[2]
-	else:
-		print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
-		print 'example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4'
-		sys.exit(1)
+	try:
+		# Parse command line parameters.
+		sensor_args = { '11': Adafruit_DHT.DHT11,
+						'22': Adafruit_DHT.DHT22,
+						'2302': Adafruit_DHT.AM2302 }
+		if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+			sensor = sensor_args[sys.argv[1]]
+			pin = sys.argv[2]
+		else:
+			print 'usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#'
+			print 'example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4'
+			sys.exit(1)
 
-	# Try to grab a sensor reading.  Use the read_retry method which will retry up
-	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+		# Try to grab a sensor reading.  Use the read_retry method which will retry up
+		# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-	# Un-comment the line below to convert the temperature to Fahrenheit.
-	# temperature = temperature * 9/5.0 + 32
+		# Un-comment the line below to convert the temperature to Fahrenheit.
+		# temperature = temperature * 9/5.0 + 32
 
-	# Note that sometimes you won't get a reading and
-	# the results will be null (because Linux can't
-	# guarantee the timing of calls to read the sensor).
-	# If this happens try again!
+		# Note that sometimes you won't get a reading and
+		# the results will be null (because Linux can't
+		# guarantee the timing of calls to read the sensor).
+		# If this happens try again!
 
-	if humidity is not None and temperature is not None:
+		if humidity is not None and temperature is not None:
 
-		with open(os.path.join(os.path.dirname(__file__), 'config.json')) as data_file:
-			config = json.load(data_file)
+			with open(os.path.join(os.path.dirname(__file__), 'config.json')) as data_file:
+				config = json.load(data_file)
 
-		todhtstorage(config, humidity, temperature)
-		toadaio(config, humidity, temperature)
-		# print 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
-	else:
-		print 'Failed to get reading. Try again!'
-		sys.exit(1)
+			todhtstorage(config, humidity, temperature)
+			toadaio(config, humidity, temperature)
+			# print 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
+		else:
+			print 'Failed to get reading. Try again!'
+			sys.exit(1)
+	except Exception as ex:
+		print ex
 
 	try:
 		unlock()
